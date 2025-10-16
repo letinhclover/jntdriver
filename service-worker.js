e272373e22aacb30c483455caa44289d2bd8e81e
letinhclover <57601291+letinhclover@users.noreply.github.com>
2025-10-17 01:53:16 +0700

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,39 +1,32 @@
-const CACHE_NAME = "jntdriver-v13-final";
+
+const CACHE_NAME = 'jntdriver-v13';
 const urlsToCache = [
-  "./",
-  "./index.html",
-  "./manifest.json",
-  "./icon.png"
+  './',
+  './index.html',
+  './manifest.json',
+  './android-chrome-192x192.png',
+  './android-chrome-512x512.png',
+  './favicon.ico',
+  './favicon-16x16.png',
+  './favicon-32x32.png'
 ];
 
-self.addEventListener("install", (event) => {
+self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(urlsToCache);
-    })
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
   );
-  console.log("🧱 Service Worker: Đã cài đặt và cache file");
 });
 
-self.addEventListener("fetch", (event) => {
+self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
-    })
+    caches.match(event.request).then(response => response || fetch(event.request))
   );
 });
 
-self.addEventListener("activate", (event) => {
+self.addEventListener('activate', event => {
   event.waitUntil(
-    caches.keys().then((cacheNames) => {
-      return Promise.all(
-        cacheNames.map((name) => {
-          if (name !== CACHE_NAME) {
-            console.log("🧹 Xóa cache cũ:", name);
-            return caches.delete(name);
-          }
-        })
-      );
-    })
+    caches.keys().then(cacheNames => Promise.all(
+      cacheNames.filter(name => name !== CACHE_NAME).map(name => caches.delete(name))
+    ))
   );
 });
