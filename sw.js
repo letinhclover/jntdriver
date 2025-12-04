0672775d42e4565e4dfccc4f995170a43e769597
letinhclover <57601291+letinhclover@users.noreply.github.com>
2025-12-04 14:05:18 +0700

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,16 @@
+const CACHE_NAME = "jnt-driver-v3-final";
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./favicon.ico"
+];
+
+self.addEventListener("install", (e) => {
+  e.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
+});
+
+self.addEventListener("fetch", (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => response || fetch(e.request))
+  );
+});
